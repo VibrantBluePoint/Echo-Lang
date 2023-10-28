d1e30c2cc6038f50d32106a75dc530c82878bc99
BluePoint <70915454+VibrantBluePoint@users.noreply.github.com>
2023-10-28 14:31:55 +0000

    examples plus extentions

---
+++ echo.py
@@ -0,0 +1,9 @@
+##########
+# Tokens #
+##########
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+        
---
+++ extentions/vscode/readme.md
@@ -0,0 +1,5 @@
+# Visual Studio Code Support for Echo
+This includes support for Echo in VSCode.
+
+### Features:
+- Syntax Highlighting
